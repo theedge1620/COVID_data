b36d04968a8e07009ef36adcd487133953c60847
theedge1620 <60548038+theedge1620@users.noreply.github.com>
2020-07-26 12:29:06 -0400

    Create get_worldometer_us.py

---
+++ get_worldometer_us.py
@@ -0,0 +1,23 @@
+#! python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  5 15:40:54 2017
+
+@author: JBC4
+
+This program scrapes worldometers for the US and outputs two files, one for 
+current data (today) and one for the previous day (yesterday)
+
+"""
+
+import requests
+import pandas as pd
+
+regwebAddress = 'https://www.worldometers.info/coronavirus/country/us/'
+
+res = requests.get(regwebAddress)
+res.raise_for_status()
+df_list=pd.read_html(res.text)
+table = df_list[0]
+table2 = df_list[1]
+
