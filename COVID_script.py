047046760cee13f0d18aa695fc3361e817729a9e
Jason Carneal <jason.carneal@protonmail.com>
2020-11-02 07:10:51 -0500

    removing unecessary output data

--- COVID_script.py
+++ COVID_script.py
@@ -9,7 +9,7 @@
 # 
 # The following code assumes you have downloaded the JHU CSSE data and placed this file in a directory directly above the cloned or downloaded repository.
 
-# In[1]:
+# In[8]:
 
 
 # Import pandas and read in latest US confirmed and death time history data:
@@ -18,12 +18,16 @@ import pandas as pd
 covid_US_confirmed = pd.read_csv("./COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv")
 covid_US_deaths = pd.read_csv("./COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv")
 
+# drop all but the last two weeks of data:
+covid_US_confirmed.drop(covid_US_confirmed.iloc[:, 11:-14], inplace = True, axis = 1)
+covid_US_deaths.drop(covid_US_deaths.iloc[:, 11:-14], inplace = True, axis = 1)
+
 
 # #### Calculate daily and weekly totals:
 # 
 # The following code calculates the daily and weekly totals for confirmed cases and deaths, since the JHU data always appends the current days date as the last column in their dataset.
 
-# In[2]:
+# In[9]:
 
 
 new_cases_US_daily = covid_US_confirmed[covid_US_confirmed.columns[-1]]-covid_US_confirmed[covid_US_confirmed.columns[-2]]
@@ -32,7 +36,7 @@ new_deaths_US_daily = covid_US_deaths[covid_US_deaths.columns[-1]]-covid_US_deat
 new_deaths_US_weekly = covid_US_deaths[covid_US_deaths.columns[-1]]-covid_US_deaths[covid_US_deaths.columns[-8]]
 
 
-# In[3]:
+# In[10]:
 
 
 # Print totals as a check:
@@ -43,7 +47,7 @@ print('Total new cases in US in last 24 hours:  ' + str(new_deaths_US_daily.sum(
 print('Total new cases in US in last 7 days  :  ' + str(new_deaths_US_weekly.sum()))
 
 
-# In[4]:
+# In[11]:
 
 
 #  Append the new data to the dataframes for confirmed cases and deaths:
@@ -53,7 +57,7 @@ covid_US_deaths['New Deaths Daily']=new_deaths_US_daily
 covid_US_deaths['New Deaths Weekly']=new_deaths_US_weekly
 
 
-# In[5]:
+# In[12]:
 
 
 # Write the new files to a .csv file that has the calculated fields as the last columns:
