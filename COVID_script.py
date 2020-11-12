0c6344bdca5d8e5a96b4be56f5002dd39d1d2bc9
Jason Carneal <jason.carneal@protonmail.com>
2020-11-12 09:30:16 -0500

    Adding updated scripts and batch

--- COVID_script.py
+++ COVID_script.py
@@ -9,7 +9,7 @@
 # 
 # The following code assumes you have downloaded the JHU CSSE data and placed this file in a directory directly above the cloned or downloaded repository.
 
-# In[8]:
+# In[1]:
 
 
 # Import pandas and read in latest US confirmed and death time history data:
@@ -27,7 +27,7 @@ covid_US_deaths.drop(covid_US_deaths.iloc[:, 11:-14], inplace = True, axis = 1)
 # 
 # The following code calculates the daily and weekly totals for confirmed cases and deaths, since the JHU data always appends the current days date as the last column in their dataset.
 
-# In[9]:
+# In[2]:
 
 
 new_cases_US_daily = covid_US_confirmed[covid_US_confirmed.columns[-1]]-covid_US_confirmed[covid_US_confirmed.columns[-2]]
@@ -36,18 +36,18 @@ new_deaths_US_daily = covid_US_deaths[covid_US_deaths.columns[-1]]-covid_US_deat
 new_deaths_US_weekly = covid_US_deaths[covid_US_deaths.columns[-1]]-covid_US_deaths[covid_US_deaths.columns[-8]]
 
 
-# In[10]:
+# In[3]:
 
 
 # Print totals as a check:
 
 print('Total new cases in US in last 24 hours:  ' + str(new_cases_US_daily.sum()))
 print('Total new cases in US in last 7 days  :  ' + str(new_cases_US_weekly.sum()))
-print('Total new cases in US in last 24 hours:  ' + str(new_deaths_US_daily.sum()))
-print('Total new cases in US in last 7 days  :  ' + str(new_deaths_US_weekly.sum()))
+print('Total new deaths in US in last 24 hours:  ' + str(new_deaths_US_daily.sum()))
+print('Total new death in US in last 7 days  :  ' + str(new_deaths_US_weekly.sum()))
 
 
-# In[11]:
+# In[4]:
 
 
 #  Append the new data to the dataframes for confirmed cases and deaths:
@@ -57,7 +57,7 @@ covid_US_deaths['New Deaths Daily']=new_deaths_US_daily
 covid_US_deaths['New Deaths Weekly']=new_deaths_US_weekly
 
 
-# In[12]:
+# In[5]:
 
 
 # Write the new files to a .csv file that has the calculated fields as the last columns:
---
+++ update_data.bat
@@ -0,0 +1,5 @@
+cd COVID-19
+git pull upstream master
+cd..
+python COVID_script.py
+pause
